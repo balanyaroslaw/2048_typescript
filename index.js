41813c19d6ce363c598c51e816d1897dffb5afb5
Yaroslav Balan <balanyaroslaw@gmail.com>
2024-03-04 12:17:54 +0200

    End game logic

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ var Events;
     Events["ArrowLeft"] = "ArrowLeft";
 })(Events || (Events = {}));
 let field = [];
+let stateField = [];
 const N = 4;
+let currentScore = 0;
+let xDown = null;
+let yDown = null;
 class Logic {
     constructor() {
+        this.field = new Field;
         this.swipePremission = true;
+        this.loseGame = false;
+        this.winGame = false;
         this.emptyCells = [];
+        this.service = new Service;
     }
     FindEmptyCell() {
         this.emptyCells = [];
@@ -61,6 +69,7 @@ class Logic {
                         }
                         else if (field[i - 1][j] !== 0 && field[i][j] !== 0 && field[i - 1][j] === field[i][j]) {
                             field[i - 1][j] = field[i - 1][j] + field[i][j];
+                            currentScore = currentScore + field[i - 1][j];
                             field[i][j] = 0;
                             isMoved++;
                         }
@@ -91,6 +100,7 @@ class Logic {
                         }
                         else if (field[i][j] !== 0 && field[i - 1][j] !== 0 && field[i][j] === field[i - 1][j]) {
                             field[i][j] = field[i][j] + field[i - 1][j];
+                            currentScore = currentScore + field[i][j];
                             field[i - 1][j] = 0;
                             isMoved++;
                         }
@@ -121,6 +131,7 @@ class Logic {
                         }
                         else if (field[i][j] !== 0 && field[i][j - 1] !== 0 && field[i][j] === field[i][j - 1]) {
                             field[i][j] = field[i][j] + field[i][j - 1];
+                            currentScore = currentScore + field[i][j];
                             field[i][j - 1] = 0;
                             isMoved++;
                         }
@@ -150,6 +161,7 @@ class Logic {
                     }
                     else if (field[i][j - 1] !== 0 && field[i][j] !== 0 && field[i][j - 1] === field[i][j]) {
                         field[i][j - 1] = field[i][j - 1] + field[i][j];
+                        currentScore = currentScore + field[i][j - 1];
                         field[i][j] = 0;
                         isMoved++;
                     }
@@ -161,17 +173,20 @@ class Logic {
         }
         this.PremissionCondition(isMoved, isNotMoved);
     }
-    CheckState() {
+    CheckStatus() {
         let fullCells = 0;
         for (let i = 0; i < N; i++) {
             for (let j = 0; j < N; j++) {
                 if (field[i][j] !== 0) {
                     fullCells++;
                 }
-                if (fullCells === 16) {
-                    alert('End');
+                if (field[i][j] === 2048) {
+                    this.winGame = true;
                 }
             }
+        }
+        if (fullCells === 16 && this.service.hasUniqueNeighbors(field)) {
+            this.loseGame = true;
         }
     }
     CreateNew() {
@@ -180,39 +195,76 @@ class Logic {
             this.AddNewBlock();
         }
     }
+    FieldState(field) {
+        let state = this.service.Clone(field);
+        if (stateField.length >= 2) {
+            stateField.pop();
+        }
+        stateField.push(state);
+    }
+    CheckRecord(score) {
+        if (localStorage.getItem('game_record') === null) {
+            localStorage.setItem('game_record', score.toString());
+        }
+        else {
+            let existedRecord = localStorage.getItem('game_record');
+            if (score > parseInt(existedRecord)) {
+                localStorage.removeItem('game_record');
+                localStorage.setItem('game_record', score.toString());
+            }
+            else {
+                localStorage.removeItem('game_record');
+                localStorage.setItem('game_record', existedRecord);
+            }
+        }
+    }
 }
 class Controller {
     Controll(event) {
         const logic = new Logic;
         const visualization = new Visualization;
         if (event.key in Events) {
-            switch (event.key) {
-                case Events.ArrowUp:
-                    {
-                        logic.SwipeUp();
-                        break;
-                    }
-                case Events.ArrowDown:
-                    {
-                        logic.SwipeDown();
-                        break;
-                    }
-                case Events.ArrowRight:
-                    {
-                        logic.SwipeRight();
-                        break;
-                    }
-                case Events.ArrowLeft:
-                    {
-                        logic.SwipeLeft();
-                        break;
-                    }
+            logic.FieldState(field);
+            logic.CheckStatus();
+            if (!logic.loseGame && !logic.winGame) {
+                switch (event.key) {
+                    case Events.ArrowUp:
+                        {
+                            logic.SwipeUp();
+                            break;
+                        }
+                    case Events.ArrowDown:
+                        {
+                            logic.SwipeDown();
+                            break;
+                        }
+                    case Events.ArrowRight:
+                        {
+                            logic.SwipeRight();
+                            break;
+                        }
+                    case Events.ArrowLeft:
+                        {
+                            logic.SwipeLeft();
+                            break;
+                        }
+                }
+                logic.CheckStatus();
+                if (logic.swipePremission) {
+                    logic.FindEmptyCell();
+                    logic.AddNewBlock();
+                }
             }
-            if (logic.swipePremission) {
-                logic.FindEmptyCell();
-                logic.AddNewBlock();
+            else if (logic.loseGame) {
+                visualization.gameLoseStateVisualization();
+                logic.CheckRecord(currentScore);
             }
-            logic.CheckState();
+            else if (logic.winGame) {
+                visualization.gameWinStateVisualization();
+                logic.CheckRecord(currentScore);
+            }
+            logic.CheckRecord(currentScore);
+            visualization.Score();
             visualization.Visualization();
         }
     }
@@ -220,7 +272,6 @@ class Controller {
 class Field {
     constructor() {
         this.field = [];
-        this.n = N;
     }
     CreateField() {
         for (let i = 0; i < N; i++) {
@@ -240,18 +291,23 @@ class Visualization {
         this.service = new Service;
     }
     Visualization() {
-        let fieldString = '';
         for (let i = 0; i < N; i++) {
             for (let j = 0; j < N; j++) {
                 if (field[i][j] !== 0) {
                     document.getElementsByClassName(this.service.joinHTMLClassName(i, j))[0].innerHTML = field[i][j].toString();
                     this.valuesColors.map(vc => {
                         if (vc.value === field[i][j]) {
-                            document.getElementsByClassName(this.service.joinHTMLClassName(i, j))[0].setAttribute("style", `background-color:${vc.color}`);
+                            document.getElementsByClassName(this.service.joinHTMLClassName(i, j))[0].setAttribute("style", `background-color:${vc.color};
+                            opacity:1;
+                            transition-property: opacity;
+                            transition-duration: 0.5s;`);
                         }
                     });
                     if (field[i][j] >= 128) {
-                        document.getElementsByClassName(this.service.joinHTMLClassName(i, j))[0].setAttribute("style", "background-color:#E7CD70");
+                        document.getElementsByClassName(this.service.joinHTMLClassName(i, j))[0].setAttribute("style", `background-color:#E7CD70;
+                        opacity:1;
+                        transition-property: opacity;
+                        transition-duration: 0.5s;`);
                     }
                 }
                 else {
@@ -259,6 +315,41 @@ class Visualization {
                     document.getElementsByClassName(this.service.joinHTMLClassName(i, j))[0].setAttribute("style", "background-color:#CDC1B3");
                 }
             }
+        }
+    }
+    createButton(menuText, buttonText) {
+        const moduleWindow = document.querySelector('.menu');
+        moduleWindow.innerHTML = menuText;
+        moduleWindow['style'].visibility = "visible";
+        const button = document.createElement('span');
+        button.innerHTML += buttonText;
+        button.classList.add('button');
+        if (buttonText === 'RESTART') {
+            button.addEventListener('click', (e) => { location.reload(); }, false);
+        }
+        else if (buttonText === 'RESUME') {
+            button.addEventListener('click', (e) => { moduleWindow['style'].visibility = "hidden"; }, false);
+        }
+        moduleWindow.appendChild(button);
+    }
+    gameWinStateVisualization() {
+        this.createButton("YOU WIN", "RESUME");
+    }
+    gameLoseStateVisualization() {
+        this.createButton("YOU LOSE", "RESTART");
+    }
+    Score() {
+        let score = document.querySelector('.score__value');
+        score.innerHTML = currentScore.toString();
+    }
+    Record() {
+        let record = document.querySelector('.record__value');
+        record.innerHTML = localStorage.getItem('game_record');
+    }
+    ReturnVisualization() {
+        if (stateField.length > 0) {
+            field = stateField[stateField.length - 1];
+            this.Visualization();
         }
     }
 }
@@ -274,6 +365,41 @@ class Service {
         let HTMLClassName = `cell ${i}_${j}`;
         return HTMLClassName;
     }
+    Clone(field) {
+        let state = [];
+        for (let i = 0; i < N; i++) {
+            state[i] = [];
+            for (let j = 0; j < N; j++) {
+                state[i][j] = field[i][j];
+            }
+        }
+        return state;
+    }
+    hasUniqueNeighbors(field) {
+        const numRows = field.length;
+        const numCols = field[0].length;
+        const hasSameNeighbors = (row, col, value) => {
+            for (let i = Math.max(0, row - 1); i <= Math.min(row + 1, numRows - 1); i++) {
+                for (let j = Math.max(0, col - 1); j <= Math.min(col + 1, numCols - 1); j++) {
+                    if (i !== row || j !== col) {
+                        if (field[i][j] === value && field[i][j] !== 0 && value !== 0) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        };
+        for (let row = 0; row < numRows; row++) {
+            for (let col = 0; col < numCols; col++) {
+                const value = field[row][col];
+                if (hasSameNeighbors(row, col, value)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
 class Game {
     constructor() {
@@ -284,10 +410,22 @@ class Game {
         this.field.CreateField();
         this.logic.CreateNew();
         this.visualization.Visualization();
+        this.BackReturn();
+        this.Restart();
+        this.visualization.Record();
     }
-    Start() {
+    BackReturn() {
+        const returnButton = document.querySelector('.return__back');
+        returnButton.addEventListener("click", (e) => { this.visualization.ReturnVisualization(); });
+    }
+    Restart() {
+        this.logic.CheckRecord(currentScore);
+        const restartButton = document.querySelector('.restart');
+        restartButton.addEventListener("click", (e) => { location.reload(); });
+    }
+    Process() {
         document.addEventListener('keydown', this.controller.Controll);
     }
 }
 const game = new Game();
-game.Start();
+game.Process();
--- index.ts
+++ index.ts
@@ -22,14 +22,29 @@ enum Events
 
 let field:TField = []
 
+let stateField:Array<TField> = [];
+
 const N:number = 4;
+
+let currentScore:number = 0
+
+let xDown = null;                                                        
+let yDown = null;       
 
 class Logic
 {
 
-    public swipePremission = true
+    private field:Field = new Field
+
+    public swipePremission:boolean = true
+
+    public loseGame:boolean = false
+
+    public winGame:boolean = false
 
     private emptyCells:Array<ICell> = [];
+
+    private service:Service = new Service
 
     public FindEmptyCell():void
     {
@@ -102,6 +117,7 @@ class Logic
                         else if(field[i-1][j] !== 0 && field[i][j] !== 0 && field[i-1][j]===field[i][j])
                         {
                             field[i-1][j] = field[i-1][j] + field[i][j];
+                            currentScore = currentScore + field[i-1][j] 
                             field[i][j] = 0;
                             isMoved++
                         }
@@ -142,6 +158,7 @@ class Logic
                         else if(field[i][j] !== 0 && field[i-1][j]!==0 && field[i][j] === field[i-1][j])
                         {
                             field[i][j] = field[i][j] + field[i-1][j]
+                            currentScore = currentScore + field[i][j]
                             field[i-1][j] = 0
                             isMoved++
                         }
@@ -181,6 +198,7 @@ class Logic
                         else if(field[i][j]!==0 && field[i][j-1]!==0 && field[i][j] === field[i][j-1])
                         {
                             field[i][j] = field[i][j] + field[i][j-1]
+                            currentScore = currentScore +  field[i][j]
                             field[i][j-1] = 0
                             isMoved++
                         }
@@ -218,6 +236,7 @@ class Logic
                     else if(field[i][j-1] !== 0 && field[i][j] !== 0 && field[i][j-1] === field[i][j])
                     {
                         field[i][j-1] = field[i][j-1] + field[i][j]
+                        currentScore = currentScore + field[i][j-1]
                         field[i][j] = 0
                         isMoved++
                     }
@@ -231,9 +250,10 @@ class Logic
         this.PremissionCondition(isMoved, isNotMoved)
     }
 
-    public CheckState():void
+    public CheckStatus():void
     {
         let fullCells:number = 0;
+        
         for(let i = 0; i < N; i++)
         {
             for(let j = 0; j < N; j++)
@@ -242,12 +262,15 @@ class Logic
                 {
                     fullCells++
                 }
-                if(fullCells===16)
-                {   
-                    alert('End')
+                if(field[i][j] === 2048)
+                {
+                    this.winGame = true
                 }
-                
             }
+        }
+        if(fullCells===16 && this.service.hasUniqueNeighbors(field))
+        {
+            this.loseGame = true
         }
     }
 
@@ -259,55 +282,101 @@ class Logic
             this.AddNewBlock()
         }
     }
-    
+
+    public FieldState(field:TField):void
+    {
+        let state = this.service.Clone(field)
+        if(stateField.length>=2)
+        {
+            stateField.pop()
+        }
+        stateField.push(state)
+    }
+
+    public CheckRecord(score:number):void
+    {
+        if(localStorage.getItem('game_record') === null)
+        {
+            localStorage.setItem('game_record', score.toString());
+        }
+        else
+        {
+            let existedRecord:string = localStorage.getItem('game_record')!
+            if(score > parseInt(existedRecord))
+            {
+                localStorage.removeItem('game_record')
+                localStorage.setItem('game_record', score.toString());
+            }
+            else
+            {
+                localStorage.removeItem('game_record')
+                localStorage.setItem('game_record', existedRecord);
+            }
+        }
+    }
 }
 class Controller
 {
     public Controll(event:KeyboardEvent):void
     {
+        
         const logic:Logic = new Logic
-    
         const visualization:Visualization = new Visualization
         if(event.key in Events)
-        {
-            switch(event.key)
+        {   
+            logic.FieldState(field)
+            logic.CheckStatus()
+            if(!logic.loseGame && !logic.winGame)
             {
-                case Events.ArrowUp:
+                switch(event.key)
                 {
-                    logic.SwipeUp()
-                    break;
-                }
-                case Events.ArrowDown:
+                    case Events.ArrowUp:
+                    {
+                        logic.SwipeUp()
+                        break;
+                    }
+                    case Events.ArrowDown:
+                    {
+                        logic.SwipeDown()
+                        break;
+                    }
+                    case Events.ArrowRight:
+                    {
+                        logic.SwipeRight()
+                        break
+                    }
+                    case Events.ArrowLeft:
+                    {
+                        logic.SwipeLeft()
+                        break
+                    }
+                } 
+                logic.CheckStatus()
+                if(logic.swipePremission)
                 {
-                    logic.SwipeDown()
-                    break;
-                }
-                case Events.ArrowRight:
-                {
-                    logic.SwipeRight()
-                    break
-                }
-                case Events.ArrowLeft:
-                {
-                    logic.SwipeLeft()
-                    break
-                }
-            }   
-            if(logic.swipePremission)
-            {
-                logic.FindEmptyCell()
-                logic.AddNewBlock()
+                    logic.FindEmptyCell()
+                    logic.AddNewBlock()
+                } 
             }
-            logic.CheckState()
+            else if(logic.loseGame)
+            {
+                visualization.gameLoseStateVisualization()
+                logic.CheckRecord(currentScore)
+            }
+            else if(logic.winGame)
+            {
+                visualization.gameWinStateVisualization()
+                logic.CheckRecord(currentScore)
+            }
+            logic.CheckRecord(currentScore)
+            visualization.Score()
             visualization.Visualization()
         }
-
     }
 }
 class Field 
 {
     private field:TField = [];
-    private readonly n:number = N;
 
     public CreateField():void
     {
@@ -331,7 +400,6 @@ class Visualization
     private service:Service = new Service
     public Visualization():void
     {
-        let fieldString:string = ''
         for(let i = 0; i < N; i++)
         {
             for(let j = 0; j < N; j++)
@@ -342,12 +410,21 @@ class Visualization
                     this.valuesColors.map(vc=>{
                         if(vc.value===field[i][j])
                         {
-                            document.getElementsByClassName(this.service.joinHTMLClassName(i,j))[0].setAttribute("style",`background-color:${vc.color}`);
+                            document.getElementsByClassName(this.service.joinHTMLClassName(i,j))[0].setAttribute("style",
+                            `background-color:${vc.color};
+                            opacity:1;
+                            transition-property: opacity;
+                            transition-duration: 0.5s;`);
+
                         }
                     })
                     if(field[i][j]>=128)
                     {
-                        document.getElementsByClassName(this.service.joinHTMLClassName(i,j))[0].setAttribute("style","background-color:#E7CD70");
+                        document.getElementsByClassName(this.service.joinHTMLClassName(i,j))[0].setAttribute("style",
+                        `background-color:#E7CD70;
+                        opacity:1;
+                        transition-property: opacity;
+                        transition-duration: 0.5s;`);
                     }
                 }
                 else
@@ -356,6 +433,62 @@ class Visualization
                     document.getElementsByClassName(this.service.joinHTMLClassName(i,j))[0].setAttribute("style","background-color:#CDC1B3");
                 }
             }
+        }
+    }
+    private createButton(menuText:string, buttonText:string):void
+    {
+        const moduleWindow:HTMLElement = document.querySelector<HTMLElement>('.menu')!;
+
+        moduleWindow.innerHTML = menuText
+        moduleWindow['style'].visibility = "visible";
+
+        const button = document.createElement('span')
+        button.innerHTML += buttonText
+        button.classList.add('button')
+        if(buttonText === 'RESTART')
+        {
+            button.addEventListener('click', (e:Event) =>{location.reload()}, false)
+        }
+        else if(buttonText === 'RESUME')
+        {
+            button.addEventListener('click', (e:Event) =>{moduleWindow['style'].visibility = "hidden";}, false)
+        }
+
+        moduleWindow.appendChild(button);
+
+    }
+
+    public gameWinStateVisualization():void
+    {
+        this.createButton("YOU WIN", "RESUME")
+    }
+
+    public gameLoseStateVisualization():void
+    {
+        this.createButton("YOU LOSE", "RESTART")
+    }
+
+    public Score():void
+    {
+        let score:HTMLElement = document.querySelector('.score__value')!;
+
+        score.innerHTML = currentScore.toString()
+    }
+
+    public Record():void
+    {
+        let record:HTMLElement = document.querySelector('.record__value')!;
+
+        record.innerHTML = localStorage.getItem('game_record')!
+    }
+
+
+    public ReturnVisualization():void
+    {
+        if(stateField.length>0)
+        {
+            field = stateField[stateField.length - 1]; 
+            this.Visualization()   
         }
     }
 }
@@ -376,6 +509,53 @@ class Service
         let HTMLClassName:string = `cell ${i}_${j}`
         return HTMLClassName
     }
+
+    public Clone(field:TField):TField
+    {
+        let state:TField = []
+        for(let i = 0; i < N; i++)
+        {
+            state[i] = [];
+            for(let j = 0; j < N; j++)
+            {
+                state[i][j] = field[i][j]
+            }
+        }
+
+        return state
+    }
+    public hasUniqueNeighbors(field: TField): boolean {
+        const numRows: number = field.length;
+        const numCols: number = field[0].length;
+    
+       const hasSameNeighbors = (row: number, col: number, value: number): boolean => 
+       {
+            for (let i: number = Math.max(0, row - 1); i <= Math.min(row + 1, numRows - 1); i++)
+            {
+                for (let j: number = Math.max(0, col - 1); j <= Math.min(col + 1, numCols - 1); j++)
+                {
+                    if (i !== row || j !== col) {
+                        if (field[i][j] === value && field[i][j] !== 0 && value !== 0) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        for (let row: number = 0; row < numRows; row++) {
+            for (let col: number = 0; col < numCols; col++) {
+                const value: number = field[row][col];
+                if (hasSameNeighbors(row, col, value)) {
+                    return false;
+                }
+            }
+        }
+    
+        return true; 
+    }
+    
 }
 class Game
 {
@@ -385,18 +565,34 @@ class Game
     
     private field:Field = new Field
 
-    private visualization = new Visualization
+    private visualization:Visualization = new Visualization
 
     constructor()
     {   
         this.field.CreateField()
         this.logic.CreateNew()
         this.visualization.Visualization()
+        this.BackReturn()
+        this.Restart()
+        this.visualization.Record()
     }
-    public Start():void
+    private BackReturn():void
+    {
+        const returnButton:HTMLElement = document.querySelector<HTMLElement>('.return__back')!;
+        returnButton.addEventListener("click", (e:Event) =>{this.visualization.ReturnVisualization()})
+    }
+
+    private Restart():void
+    {
+        this.logic.CheckRecord(currentScore)
+        const restartButton:HTMLElement = document.querySelector<HTMLElement>('.restart')!;
+        restartButton.addEventListener("click", (e:Event) =>{location.reload()})
+    }
+
+    public Process():void
     {
         document.addEventListener('keydown', this.controller.Controll)
     }
 }
 const game:Game = new Game()
-game.Start()
+game.Process()
